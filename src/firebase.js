a8714a1e2e8b233351e8bfaa18e5e1f245087d2e
HarizHaswan <mhariz.mhaswan@student.gmi.edu.my>
2026-07-12 00:54:10 +0800

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCkQtiQlCPW5csmwrqNMLl9uEyZbPKUfWk",
-  authDomain: "check-in-system-ad362.firebaseapp.com",
-  projectId: "check-in-system-ad362",
-  storageBucket: "check-in-system-ad362.firebasestorage.app",
-  messagingSenderId: "1052761621854",
-  appId: "1:1052761621854:web:db803aa741f254b472bbcf",
-  measurementId: "G-Y3KFZ2N4EC"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
